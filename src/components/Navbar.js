486681c65e1d8070defa6019d24c2f90d62a4882
Hanfried-Nguegan <80390250+Hanfried-Nguegan@users.noreply.github.com>
2023-05-27 01:54:13 +0100

    navbar commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,11 +12,9 @@ const Navbar = () => {
     setIsClicked((prevState) => !prevState);
   };
 
-  console.log("Clicked", isClicked);
-
   return (
     <header className="fixed top-0 z-50 w-full box-border">
-      <div className="flex items-center justify-between w-full p-2 max-w-7xl mx-auto">
+      <div className="flex items-center justify-between w-full p-2 max-w-[1400px] mx-auto">
         <div className="p-1 cursor-pointer">
           <Image
             src={pokemonLogo}
@@ -29,7 +27,7 @@ const Navbar = () => {
           className="flex items-center justify-center cursor-pointer "
           onClick={handleMenuClick}
         >
-          <div className="relative toggle-menu pointer-events-auto bg-pokemonYellow shadow-xl w-16 h-16 md:w-20 md:h-20 rounded-full inline-flex items-center justify-center">
+          <div className="relative toggleMenu pointer-events-auto bg-pokemonYellow shadow-xl w-16 h-16 md:w-20 md:h-20 rounded-full inline-flex items-center justify-center">
             <h2 className="uppercase text-sm font-bold text-white font-mono">
               {isClicked ? <span>Close</span> : <span>Menu</span>}
             </h2>
@@ -39,7 +37,7 @@ const Navbar = () => {
       <nav
         className={`${
           isClicked ? "navOpen " : "navClose"
-        }  navMenu w-full h-full text-right pt-6 text-white uppercase px-5 whitespace-nowrap `}
+        }  navMenu w-full h-full text-right fixed right-0 z-[-1] overflow-x-auto top-0 pt-28 md:pt-36 lg:pt-40 text-white uppercase px-5 md:px-12 lg:px-48 whitespace-nowrap pointer-events-auto leading-none `}
       >
         <p>Menu</p>
       </nav>
