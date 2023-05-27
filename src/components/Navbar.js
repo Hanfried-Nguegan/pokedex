ea736f8adff3946a6967b0a79a93baa9f2471e81
Hanfried-Nguegan <80390250+Hanfried-Nguegan@users.noreply.github.com>
2023-05-27 01:14:28 +0100

    navbar commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,8 @@ import { pokemonLogo, hamburgerMenu } from "@/images";
 
 const Navbar = () => {
   const [isClicked, setIsClicked] = useState(false);
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const [isCloseMenu, setIsCloseMenu] = useState(false);
 
   const handleMenuClick = () => {
     setIsClicked((prevState) => !prevState);
@@ -14,26 +16,33 @@ const Navbar = () => {
 
   return (
     <header className="fixed top-0 z-50 w-full box-border">
-      <div className="flex items-center justify-between w-full p-2 md:max-w-7xl mx-auto">
+      <div className="flex items-center justify-between w-full p-2 max-w-7xl mx-auto">
         <div className="p-1 cursor-pointer">
           <Image
             src={pokemonLogo}
             alt="Pokemon logo"
             priority="true"
-            className="w-[130px] h-auto md:w-[150px] lg:w-[180px]"
+            className="w-[140px] h-auto md:w-[160px] lg:w-[180px]"
           />
         </div>
         <div
           className="flex items-center justify-center cursor-pointer "
           onClick={handleMenuClick}
         >
-          <div className="relative toggle-menu pointer-events-auto bg-pokemonYellow shadow-xl border-l border-opacity-30 border-white w-16 h-16 rounded-full inline-flex items-center justify-center">
+          <div className="relative toggle-menu pointer-events-auto bg-pokemonYellow shadow-xl w-16 h-16 md:w-20 md:h-20 rounded-full inline-flex items-center justify-center">
             <h2 className="uppercase text-sm font-bold text-white font-mono">
-              Menu
+              {isClicked ? <span>Close</span> : <span>Menu</span>}
             </h2>
           </div>
         </div>
       </div>
+      <nav
+        className={`${
+          isClicked ? "navOpen " : "navClose"
+        }  navMenu w-full h-full text-right pt-6 text-white uppercase px-5 whitespace-nowrap `}
+      >
+        <p>Menu</p>
+      </nav>
     </header>
   );
 };
