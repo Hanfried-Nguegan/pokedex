c0e6140ca8854a65476e13f4430b68a80246fdb6
Hanfried-Nguegan <80390250+Hanfried-Nguegan@users.noreply.github.com>
2023-05-28 21:39:18 +0100

    searchbar commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,21 +4,23 @@ import { pokeball, searchIcon, snorlax } from "@/images";
 
 const SearchBar = () => {
   return (
-    <div className="fixed bottom-0 left-0 w-full z-10 bg-white shadow-lg drop-shadow-lg">
-      <div className="relative flex items-center justify-between p-2">
-        <div className="absolute -top-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <Image src={snorlax} alt="snorlax image" />
-        </div>
-        <div>
-          <Image src={pokeball} alt="pokeball image" width={70} />
-        </div>
-        <input
-          type="text"
-          placeholder="Search by Pokemon name or id number"
-          className="flex flex-1 m-2 p-2 font-pokemonSolid text-sm outline-none"
-        />
-        <div>
-          <Image src={searchIcon} alt="search icon" width={35} />
+    <div className="fixed flex items-center justify-center bottom-0 left-0 w-full p-2 z-50">
+      <div className="w-full md:w-[60%] lg:w-[70%] bg-white rounded-full shadow-lg drop-shadow-lg">
+        <div className="relative flex items-center justify-between p-2">
+          <div className="absolute -top-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <Image src={snorlax} alt="snorlax image" />
+          </div>
+          <div className="cursor-pointer">
+            <Image src={pokeball} alt="pokeball image" width={70} />
+          </div>
+          <input
+            type="text"
+            placeholder="Search by name or number"
+            className="flex flex-1 m-2 p-2 font-pokemonSolid text-sm md:text-base outline-none"
+          />
+          <div className="cursor-pointer">
+            <Image src={searchIcon} alt="search icon" width={30} />
+          </div>
         </div>
       </div>
     </div>
